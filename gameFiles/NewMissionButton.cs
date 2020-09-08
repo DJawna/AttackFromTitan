19c195302e13f4156ca93639636ce818c3d6c9c2
daniel <djawna@gmail.com>
2020-09-08 21:48:34 +0200

    add level 1

---
+++ gameFiles/NewMissionButton.cs
@@ -0,0 +1,20 @@
+using Godot;
+using System;
+
+public class NewMissionButton : Button
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+	private void _on_NewMissionButton_button_down()
+	{
+		GetTree().ChangeScene("res://Levels/Level1.tscn");   
+    }
+}
