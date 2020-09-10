fccc00f0e424c86046b636f17002cdd589bca9bb
daniel <djawna@gmail.com>
2020-09-10 22:45:02 +0200

    pause screen implemented

---
+++ gameFiles/Levels/PauseButtonListener.cs
@@ -0,0 +1,32 @@
+using Godot;
+using System;
+
+public class PauseButtonListener : Node2D
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        
+    }
+
+    public override void _Input(InputEvent inputEvent)
+    {
+        if (inputEvent.IsActionPressed("pause"))
+        {
+            if (GetTree().Paused){
+                this.Hide();
+                GetTree().Paused = true;
+            }else{
+                
+                GetTree().Paused = false;
+                this.Show();
+            }
+            
+            
+        }
+    }
+}
