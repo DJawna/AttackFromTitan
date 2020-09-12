f2073bc8b875f4bfedd868c315dc13ad7a61a7d7
daniel <djawna@gmail.com>
2020-09-12 14:41:55 +0200

    scriptsConsolidation Part1

--- gameFiles/NewMissionButton.cs
+++ gameFiles/NewMissionButton.cs
@@ -1,18 +1,21 @@
 using Godot;
 using System;
 
-public class NewMissionButton : Button
-{
-    // Declare member variables here. Examples:
-    // private int a = 2;
-    // private string b = "text";
+namespace AttackFromTitan.Core{
+    public class NewMissionButton : Button
+    {
+        // Declare member variables here. Examples:
+        // private int a = 2;
+        // private string b = "text";
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready() {
+        // Called when the node enters the scene tree for the first time.
+        public override void _Ready() {
 
-    }
+        }
 
-    private void _on_NewMissionButton_button_down() {
-        GetTree().ChangeScene("res://Levels/Level1.tscn");
+        private void _on_NewMissionButton_button_down() {
+            GetTree().ChangeScene("res://Levels/Level1.tscn");
+        }
     }
 }
+
