56c9b1e6e33b7440b75edb3ca564377de5c2c820
daniel <djawna@gmail.com>
2020-09-13 23:43:08 +0200

    add playercontrolled ship movement

--- gameFiles/Scripts/Core/PlayerControlledShip.cs
+++ gameFiles/Scripts/Core/PlayerControlledShip.cs
@@ -7,59 +7,77 @@ namespace AttackFromTitan.Core
     {
         private Vector2 velocity = new Vector2(0, 0);
 
+
         /// <summary>
-        /// Acceleration value to be applied to the velocity   
+        /// MaxSpeed the ship can have   
         /// </summary>
         /// <value>m*s^2 min value: 0</value>
         [Export]
-        public float Acceleration { get; set; }
+        public float MaxSpeed { get; set; } = 666f;
 
-        /// <summary>
-        /// MaxVelocity the ship can have
-        /// </summary>
-        /// <value>m*s min value: 0</value>
-        [Export]
-        public float MaxVelocity { get; set; } = 666;
+        
 
-        /// <summary>
-        /// Deceleration value to be applied to the velocity   
-        /// </summary>
-        /// <value>m*s^2 min value: 0</value>
-        [Export]
-        public float Deceleration { get; set; }
 
-        private void accelerate(Vector2 direction, float deltaSecs, float force) {
-
-            velocity.x = Math.Min(velocity.x + (direction.x * Acceleration * deltaSecs * force), MaxVelocity);
-            velocity.y = Math.Min(velocity.y + (direction.y * Acceleration * deltaSecs * force), MaxVelocity);
-        }
-
+        private float currentAcceleration;
 
         //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+
         public override void _Process(float delta) {
+            var direction = new Vector2(0f,0f);
+            var currentStrength =0f;            
             if (Input.IsActionPressed("ui_up"))
             {
-                accelerate(Vector2.Up, delta, Input.GetActionStrength("ui_up"));
+                direction.x = Vector2.Up.x;
+                direction.y = Vector2.Up.y;
+
+                currentStrength = Input.GetActionStrength("ui_up");
+
             }
             if (Input.IsActionPressed("ui_down"))
             {
-                accelerate(Vector2.Down, delta, Input.GetActionStrength("ui_down"));
+                direction.x = Vector2.Down.x;
+                direction.y = Vector2.Down.y;
+
+                currentStrength = Input.GetActionStrength("ui_down");
             }
             if (Input.IsActionPressed("ui_right"))
             {
-                accelerate(Vector2.Right, delta, Input.GetActionStrength("ui_right"));
+                direction.x = Vector2.Right.x;
+                direction.y = Vector2.Right.y;
+
+                currentStrength = Input.GetActionStrength("ui_right");
             }
             if (Input.IsActionPressed("ui_left"))
             {
-                accelerate(Vector2.Left, delta, Input.GetActionStrength("ui_left"));
+                direction.x = Vector2.Left.x;
+                direction.y = Vector2.Left.y;
+
+                currentStrength = Input.GetActionStrength("ui_left");
             }
 
-            /*
-                    var newVelocity = velocity  + ( velocity.Normalized() * velocity.Sign() * Deceleration);
-                    velocity.x = Math.Max(0, newVelocity.x);
-                    velocity.y = Math.Max(0, newVelocity.y);*/
-            Position = Position + (velocity * delta);
+            // accelerate according to input
+            velocity.x += direction.x * currentStrength * 5;
+            velocity.y += direction.y * currentStrength * 5;
 
+            // decelerate according to "friction"
+            velocity.x += Math.Sign(velocity.x) * -1 * Math.Abs(velocity.x) * 0.8f * delta;
+            velocity.y += Math.Sign(velocity.y) * -1 * Math.Abs(velocity.y) * 0.8f * delta;
+
+/*
+            if(currentStrength == 0f){
+                accelerate(directionVector,delta,currentStrength,currentAcceleration );
+            }else{
+                accelerate(currentDirection, delta,currentStrength, currentAcceleration);
+            }
+*/
+            // multiply by -1 in order to invert the direction of the movement:
+            //velocity.x = Math.Max(0, velocity.x + (velocity.x * -1 * Deceleration * delta));
+            //velocity.y = Math.Max(0, velocity.y + (velocity.y * -1 * Deceleration * delta));
+            
+            var newPosition = new Vector2();
+            newPosition.x = Position.x + (velocity.x * delta);
+            newPosition.y = Position.y + (velocity.y * delta);
+            this.Position = newPosition;
         }
     }
 
